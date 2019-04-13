4f1330d19a681f779f4c1f6de367704088e4984f
KaMiNaLeX <kaminskiy_2013@mail.ru>
2019-04-13 11:04:01 +0300

    initial commit

--- src/main/java/com/example/food/controllers/OrdersController.java
+++ src/main/java/com/example/food/controllers/OrdersController.java
@@ -4,6 +4,7 @@ import com.example.food.dto.OrdersDto;
 import com.example.food.services.OrdersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,6 +13,11 @@ import java.util.List;
 public class OrdersController {
     @Autowired
     private OrdersService ordersService;
+
+    @PostMapping("/orders/create")
+    public OrdersDto createOrder() {
+        return null;
+    }
 
     @GetMapping("/orders")
     public List<OrdersDto> getAllOrders() {
