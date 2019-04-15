6c98da7e0694e56be306e06296f50f9791bbd5a6
chipallino1 <31686211+chipallino1@users.noreply.github.com>
2019-04-15 15:51:53 +0300

    bug fixing

--- src/main/java/com/example/food/repositories/MenuRepository.java
+++ src/main/java/com/example/food/repositories/MenuRepository.java
@@ -12,7 +12,7 @@ import java.util.Map;
 @Repository
 public interface MenuRepository extends JpaRepository<Menu, Long> {
     @Query(value = "SELECT D.NAME ,M.CATEGORY, M.COST FROM MENU M " +
-            "INNER JOIN DISHES D ON M.DISH_ID = D.ID", countQuery = " SELECT COUNT(*) " +
+            "INNER JOIN DISHES D ON M.DISH_ID = D.ID", countQuery = "SELECT COUNT(*) FROM MENU M " +
             "INNER JOIN DISHES D ON M.DISH_ID = D.ID ", nativeQuery = true)
     Page<Map<String, Object>> findAllDishes(Pageable pageable);
 }
